6e66878084b567449ab2a383f4e5e3e935c1c04a
Sigalit <sigudim@gmail.com>
2016-03-07 14:23:21 +0200

    Screen2 + 2 textViews +2 Buttons

--- app/src/main/java/com/sigudim/intentsig1/Screen1.java
+++ app/src/main/java/com/sigudim/intentsig1/Screen1.java
@@ -1,7 +1,10 @@
 package com.sigudim.intentsig1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Screen1 extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class Screen1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen1);
+        final Button buttonScreen2 = (Button) findViewById(R.id.button_screen2);
+        buttonScreen2.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                // Create an explicit Intent for starting the Screen2
+                // Activity
+                Intent Screen2Intent = new Intent(Screen1.this,
+                        Screen2.class);
+
+                // Use the Intent to start the HelloAndroid Activity
+                startActivity(Screen2Intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/sigudim/intentsig1/Screen2.java
+++ app/src/main/java/com/sigudim/intentsig1/Screen2.java
@@ -1,7 +1,10 @@
 package com.sigudim.intentsig1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Screen2 extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class Screen2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen2);
+        final Button buttonScreen1 = (Button) findViewById(R.id.button_screen1);
+        buttonScreen1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                // Create an explicit Intent for starting the Screen2
+                // Activity
+                Intent Screen1Intent = new Intent(Screen2.this,
+                        Screen1.class);
+
+                // Use the Intent to start the HelloAndroid Activity
+                startActivity(Screen1Intent);
+            }
+        });
     }
 }
