ff7cd92947d1b78070eae3d67faf5f0a3153c927
Sigalit <sigudim@gmail.com>
2016-03-12 08:14:19 +0200

    Screen2 + 2 textViews +2 Buttons

--- app/src/main/java/com/sigudim/intentsig1/Screen1.java
+++ app/src/main/java/com/sigudim/intentsig1/Screen1.java
@@ -5,13 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Screen1 extends AppCompatActivity {
-
+    private TextView textViewData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen1);
+
+        textViewData = (TextView)findViewById(R.id.textView_data);
+        Intent iin= getIntent();
+        Bundle b = iin.getExtras();
+
+        if(b!=null)
+        {
+            String j =(String) b.get("name");
+            textViewData.setText(j);
+        }
 
         final Button buttonScreen2 = (Button) findViewById(R.id.button_screen2);
         buttonScreen2.setOnClickListener(new View.OnClickListener() {
